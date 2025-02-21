c218c524a6449a1c27591114fa5c75eeebb08095
swethadurai <111978148+swethadurai@users.noreply.github.com>
2025-02-21 15:34:53 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 import { Toaster } from "../components/ui/toaster";
 import { Toaster as Sonner } from "../components/ui/sonner";
-=======
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
->>>>>>> first commit
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -16,11 +13,9 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-<<<<<<< HEAD
-=======
+
       <Toaster />
       <Sonner />
->>>>>>> first commit
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
