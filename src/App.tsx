649e07b53d48c5a00b2f0856f5f5db52399f31e7
Swetha <Swetha@io-priya-rnd.local>
2025-02-21 14:51:25 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 import { Toaster } from "../components/ui/toaster";
 import { Toaster as Sonner } from "../components/ui/sonner";
+=======
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+>>>>>>> first commit
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -11,6 +16,11 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+<<<<<<< HEAD
+=======
+      <Toaster />
+      <Sonner />
+>>>>>>> first commit
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
